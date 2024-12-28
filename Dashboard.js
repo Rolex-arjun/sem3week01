ebe2b835f4436985a7f4bb07dc1a1b0e594da83f
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2024-12-28 20:53:51 +0530

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Welcome to the Dashboard</h2>
+      <p>Here, you can manage your account and images.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
